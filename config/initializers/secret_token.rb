597b94867fcb7c12460934e7e8ebc3d900d669e8
Andrew Elliott <contact@awegroup.com>
2012-03-05 23:20:21 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattMiller::Application.config.secret_token = 'c09d90d949d7810f6a58ae31a262edb4e44c99a730857f45db74d82b49f076a1249d0888901f938a7a607d224cd13e712f2fe0bdb0100ec7d3b0825568adfe07'
